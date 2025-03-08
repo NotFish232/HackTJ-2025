f8e11acbc3ba1813f0cf328a0660e6f45a2a97cf
Justin Lee <justin.adam.lee@gmail.com>
2025-03-08 14:57:25 -0500

    fix tailwind

--- website/quantumfold/apps/theme/static_src/tailwind.config.js
+++ website/quantumfold/apps/theme/static_src/tailwind.config.js
@@ -14,6 +14,8 @@ module.exports = {
         /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
         '../templates/**/*.html',
 
+        '../../../../templates/**/*.html',
+
         /*
          * Main templates directory of the project (BASE_DIR/templates).
          * Adjust the following line to match your project structure.
