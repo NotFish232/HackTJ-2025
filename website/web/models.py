b19c6f6fbb50061bce903da67ebdccd1117cbde2
Justin Lee <justin.adam.lee@gmail.com>
2025-03-08 14:27:57 -0500

    add new models

--- website/web/models.py
+++ website/web/models.py
@@ -9,3 +9,17 @@ class User(AbstractBaseUser):
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     email = models.EmailField(unique=True)
+
+
+class Protein(models.Model):
+    entry_id = models.CharField(unique=True)
+
+    uniprot_id = models.CharField()
+    uniprot_sequence = models.CharField()
+    uniprot_description = models.CharField()
+
+class ProteinResult(models.Model):
+    protein = models.ForeignKey(Protein, on_delete=models.CASCADE)
+
+    alphafold_result = models.FileField()
+    quantum_result = models.FileField()
