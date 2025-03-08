9a0661e73217206ab39cdb3ba46ffd8141bb5961
Justin Lee <justin.adam.lee@gmail.com>
2025-03-08 15:24:14 -0500

    add stuff

--- website/protein_folding.py
+++ website/protein_folding.py
@@ -1,6 +1,7 @@
 import requests
 from urllib.parse import quote
-import json
+from urllib.request import urlretrieve
+from pathlib import Path
 
 import os
 
@@ -8,18 +9,26 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quantumfold.settings")
 import django
 
 django.setup()
-from quantumfold import Protein
+
+from quantumfold.apps.web.models import Protein
+
 
 ALPHAFOLD_API_PRED_URL = "https://alphafold.ebi.ac.uk/api/prediction"
+
+MEDIA_PATH = Path(__file__).parent / "media"
 
 
 SCIENTIFIC_NAME_KEY = "organismScientificName"
 HUMAN_SPECIES_VALUE = "Homo sapiens"
 
 ENTRY_ID_KEY = "entryId"
+UNIPROT_ID_KEY = "uniprotId"
+UNIPROT_DESCRIPTION_KEY = "uniprotDescription"
+UNIPROT_SEQUENCE_KEY = "uniprotSequence"
+PDBURL_KEY = "pdbUrl"
 
 
-def alphafold_fetch(protein_name: str) -> tuple[str, str, str] | None:
+def alphafold_fetch(protein_name: str) -> tuple[Protein, str] | None:
     response = requests.get(f"{ALPHAFOLD_API_PRED_URL}/{quote(protein_name)}")
     if response.status_code != 200:
         return None
@@ -34,7 +43,42 @@ def alphafold_fetch(protein_name: str) -> tuple[str, str, str] | None:
         return None
 
     entry_id = human_protein_json[ENTRY_ID_KEY]
+    uniprot_id = human_protein_json[UNIPROT_ID_KEY]
+    uniprot_description = human_protein_json[UNIPROT_DESCRIPTION_KEY]
+    uniprot_sequence = human_protein_json[UNIPROT_SEQUENCE_KEY]
 
-    return Protein.objects.create()
+    pdb_file_url = human_protein_json[PDBURL_KEY]
 
-    print(json.dumps(human_protein_json, indent=4))
+    protein = Protein(
+        entry_id=entry_id,
+        uniprot_id=uniprot_id,
+        uniprot_description=uniprot_description,
+        uniprot_sequence=uniprot_sequence,
+    )
+
+    return protein, pdb_file_url
+
+
+def run_quantum_protein_folding() -> None:
+    pass
+
+def run_full_protein_folding(protein_name: str) -> bool:
+    # get results from alphafold api
+    alphafold_result = alphafold_fetch(protein_name)
+
+    if alphafold_result is None:
+        return False
+
+    protein, pdb_file_url = alphafold_result
+
+    protein_dir = MEDIA_PATH / protein.uniprot_id
+
+    protein_dir.mkdir(exist_ok=True)
+
+    # downloads the alphafold pdb
+    urlretrieve(pdb_file_url, str(protein_dir / "alphafold.pdb"))
+
+    return True
+
+
+print(run_full_protein_folding("Angiotensin 1-10"))
